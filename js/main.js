2aa0b5dad6787a972143972420bac8ae1655b10f
Kim Rune Møller <kimrune@hotmail.no>
2022-12-13 09:09:57 +0100

    commit

--- js/main.js
+++ js/main.js
@@ -2,22 +2,22 @@ const menuButton = document.querySelector("#menu_button");
 menuButton.addEventListener("click", toggleMenu, false);
 
 const menuIcon = document.querySelector("#menu_icon");
-const nav = document.getElementById("nav");
-const backdrop = document.getElementById("backdrop");
+const menu = document.querySelector("#slidein_menu");
+
+let scrollToggled = false;
 
 let timeout = false, // holder for timeout id
     delay = 100; // delay after event is "complete" to run callback
 
 function toggleMenu() {        
-    menuIcon.classList.toggle("fa-xmark");
-    nav.classList.toggle("menu__open");
-    backdrop.classList.toggle("backdrop__show");
+    menuIcon.classList.toggle("fa-xmark");    
+    menu.classList.toggle("slidein-menu__open");
 }
 
 function collapseMenu() {
     if (window.innerWidth > 800) {
         menuIcon.classList.remove("fa-xmark");
-        nav.classList.remove("menu__open");
+        //nav.classList.remove("menu__open");
         backdrop.classList.remove("backdrop__show");        
     }
 }
@@ -35,7 +35,7 @@ source: https://www.section.io/engineering-education/watch-for-system-dark-mode-
 */
 function modeChange(e) {
     const colorScheme = e.matches ? "dark" : "light";
-
+    console.log(colorScheme);
 		if (colorScheme === "dark") {
 			document.querySelector("#logo").src = "../images/logo-darkmode.png";
 		} else {
@@ -43,5 +43,36 @@ function modeChange(e) {
 		}
 }
 
-window.matchMedia("(prefers-color-scheme: dark)").onchange = modeChange;
+
+  
+  //check this: intersection observer
+  //https://stackoverflow.com/questions/66003295/how-to-trigger-animation-when-element-is-in-view
+
+
+// document.addEventListener('scroll', (e) => {
+    
+//     if (window.scrollY > 100 && !scrollToggled) {
+//         var header = document.querySelector(".header");
+//         var subheader = document.querySelector(".subheader");
+//         if (!header.classList.contains("header-small")) {
+//             header.classList.add("header-small");
+//         }           
+//         if (subheader && !subheader.classList.contains("header-small")) {
+//             subheader.classList.add("header-small");
+//         }           
+//         scrollToggled = true;
+//     }
+//     if (window.scrollY < 100 && scrollToggled) {
+//         var header = document.querySelector(".header");
+//         var subheader = document.querySelector(".subheader");
+//         header.classList.remove("header-small");   
+//         if (subheader) {
+//             subheader.classList.remove("header-small");        
+//         }
+//         scrollToggled = false;
+//     }
+    
+//   });
+
+//window.matchMedia("(prefers-color-scheme: dark)").onchange = modeChange;
 
