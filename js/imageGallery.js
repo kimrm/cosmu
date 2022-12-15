5a0468824a4c929a5900faa2359b9ba8d6a814d4
Kim Rune Møller <kimrune@hotmail.no>
2022-12-15 22:31:42 +0100

    commit

---
+++ js/imageGallery.js
@@ -0,0 +1,112 @@
+const libraryGallery = [
+    "/library/lib1.jpg",
+    "/library/lib2.jpg",
+    "/library/lib3.jpg",
+    "/library/lib4.jpg"
+];
+
+const restaurantGallery = [
+    "/restaurant/restaurant1.jpg",
+    "/restaurant/restaurant2.jpg",
+    "/restaurant/restaurant3.jpg",
+    "/restaurant/restaurant4.jpg"
+];
+
+const shopGallery = [
+    "/shop/shop1.jpg",
+    "/shop/shop2.jpg",
+    "/shop/shop3.jpg",
+    "/shop/shop4.jpg"
+];
+
+function createGallery(gallery) {    
+    const imgView = document.querySelector("#imageView");
+    const thumbnails = document.querySelector("#galleryList");
+    thumbnails.innerHTML="";    
+    gallery.forEach(element => {
+        const img = document.createElement("img");
+        img.src = "/images/gallery" + element;
+        thumbnails.appendChild(img);
+        img.addEventListener("click", element => {
+            imgView.src = img.src;
+        });
+    });
+}
+
+function closeGallery() {
+    const backdrop = document.querySelector("#backdrop");
+    const gallery = document.querySelector("#imageGallery");
+    gallery.classList.toggle("image-gallery__show");
+    backdrop.classList.toggle("backdrop");
+}
+
+const showLibraryGallery = function() {
+    const title = document.querySelector("galleryTitle");
+    const backdrop = document.querySelector("#backdrop");
+    const gallery = document.querySelector("#imageGallery");
+
+    const imgView = document.querySelector("#imageView");
+
+    const clickedImg = this.firstChild;
+
+    console.log(clickedImg);
+    
+    imgView.src = clickedImg.src;
+    
+    gallery.classList.toggle("image-gallery__show");
+    createGallery(libraryGallery);
+    backdrop.classList.toggle("backdrop");
+    galleryTitle.innerText = "Cosmu library";
+}
+
+const showRestaurantGallery = function() {
+    const title = document.querySelector("galleryTitle");
+    const backdrop = document.querySelector("#backdrop");
+    const gallery = document.querySelector("#imageGallery");
+
+    const imgView = document.querySelector("#imageView");
+    
+    const clickedImg = this.firstChild;
+
+    imgView.src = clickedImg.src;
+    
+    gallery.classList.toggle("image-gallery__show");
+    createGallery(restaurantGallery);
+    backdrop.classList.toggle("backdrop");
+    galleryTitle.innerText = "Cosmu restaurant";
+}
+
+const showShopGallery = function() {
+    const title = document.querySelector("galleryTitle");
+    const backdrop = document.querySelector("#backdrop");
+    const gallery = document.querySelector("#imageGallery");
+
+    const imgView = document.querySelector("#imageView");
+    
+    const clickedImg = this.firstChild;
+
+    imgView.src = clickedImg.src;
+    
+    gallery.classList.toggle("image-gallery__show");
+    createGallery(shopGallery);
+    backdrop.classList.toggle("backdrop");
+    galleryTitle.innerText = "Cosmu shop";
+}
+
+document.querySelector("#libraryGalleryLink1").onclick = showLibraryGallery;
+document.querySelector("#libraryGalleryLink2").onclick = showLibraryGallery;
+document.querySelector("#libraryGalleryLink3").onclick =  showLibraryGallery;
+document.querySelector("#libraryGalleryLink4").onclick = showLibraryGallery;
+
+document.querySelector("#restaurantGalleryLink1").onclick = showRestaurantGallery;
+document.querySelector("#restaurantGalleryLink2").onclick = showRestaurantGallery;
+document.querySelector("#restaurantGalleryLink3").onclick = showRestaurantGallery;
+document.querySelector("#restaurantGalleryLink4").onclick = showRestaurantGallery;
+
+document.querySelector("#shopGalleryLink1").onclick = showShopGallery;
+document.querySelector("#shopGalleryLink2").onclick = showShopGallery;
+document.querySelector("#shopGalleryLink3").onclick = showShopGallery;
+document.querySelector("#shopGalleryLink4").onclick =  showShopGallery;
+
+document.querySelector("#galleryClose").addEventListener("click", closeGallery);
+document.querySelector("#backdrop").addEventListener("click", closeGallery);
--- js/main.js
+++ js/main.js
@@ -17,8 +17,7 @@ function toggleMenu() {
 function collapseMenu() {
     if (window.innerWidth > 800) {
         menuIcon.classList.remove("fa-xmark");
-        //nav.classList.remove("menu__open");
-        backdrop.classList.remove("backdrop__show");        
+        //nav.classList.remove("menu__open");        
     }
 }
 
@@ -30,49 +29,5 @@ function reportWindowSize() {
 window.onresize = reportWindowSize;
 
 
-/* Watch for dark/light mode change 
-source: https://www.section.io/engineering-education/watch-for-system-dark-mode-using-js-css/
-*/
-function modeChange(e) {
-    const colorScheme = e.matches ? "dark" : "light";
-    console.log(colorScheme);
-		if (colorScheme === "dark") {
-			document.querySelector("#logo").src = "../images/logo-darkmode.png";
-		} else {
-			document.querySelector("#logo").src = "../images/logo_500px.png";
-		}
-}
 
-
-  
-  //check this: intersection observer
-  //https://stackoverflow.com/questions/66003295/how-to-trigger-animation-when-element-is-in-view
-
-
-// document.addEventListener('scroll', (e) => {
-    
-//     if (window.scrollY > 100 && !scrollToggled) {
-//         var header = document.querySelector(".header");
-//         var subheader = document.querySelector(".subheader");
-//         if (!header.classList.contains("header-small")) {
-//             header.classList.add("header-small");
-//         }           
-//         if (subheader && !subheader.classList.contains("header-small")) {
-//             subheader.classList.add("header-small");
-//         }           
-//         scrollToggled = true;
-//     }
-//     if (window.scrollY < 100 && scrollToggled) {
-//         var header = document.querySelector(".header");
-//         var subheader = document.querySelector(".subheader");
-//         header.classList.remove("header-small");   
-//         if (subheader) {
-//             subheader.classList.remove("header-small");        
-//         }
-//         scrollToggled = false;
-//     }
-    
-//   });
-
-//window.matchMedia("(prefers-color-scheme: dark)").onchange = modeChange;
 
